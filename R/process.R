1ccf02d234e9478f44257617867a5a62e193d951
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2022-04-11 14:25:34 +0300

    update docs for process

--- R/process.R
+++ R/process.R
@@ -1,29 +1,34 @@
-#' Buffer sf objects in geographic coordinates through conic projection
+#' Quasi-geographic buffering
+#' 
+#' Generate the buffer of spatial object in geographic coordinates. The function transforms the object into Azimuthal equidistant projection, then buffers it by the specified radius and then reprojects into geographical coordinate system (WGS84)
 #'
-#' @param g `sf` or `sfg` object in geographic coordinates
-#' @param bufsize double. Size of a buffer, in metres
+#' @param g `sf` or `sfg` object with known coordinate system.
+#' @param bufsize Numeric value of a buffer distance, in meters.
 #'
-#' @return `sf` or `sfg` object, buffered to `bufsize` distance
+#' @return `sf` or `sfg` object, buffered to `bufsize` and projected into geographic coordinates (WGS84).
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' gr_buffer_geo(basin, 50000)
-#' }
 gr_buffer_geo <- function(g, bufsize){
-  # TODO: create geodetic buffering instead
+  box = sf::st_bbox(g)
+  lon0 = 0.5 * (box[1] + box[3]) # longitude
+  lat0 = 0.5 * (box[2] + box[4]) # latitude
+  prj = stringr::str_interp('+proj=aeqd +lat_0=${lat0} +lon_0=${lon0} +x_0=0 +y_0=0 +datum=WGS84')
+  
   g %>% 
-    st_transform_opt() %>%
+    sf::st_transform(prj) %>% 
     sf::st_buffer(bufsize) %>% 
     sf::st_transform(4326)
 }
 
 
-#' Get gaps in the data
+#' Get gaps in the runoff data
+#' 
+#' Use the function to detect periods of missing data. These can be both missing dates and missing runoff values
 #'
-#' @param hdata a data frame where the first column is date, and the second column is discharge value
+#' @param hdata `data.frame` with two columns, where the first column is `Date`, and the second column is numeric runoff value
 #'
-#' @return a data frame with periods of data and periods of gaps
+#' @return `data.frame` with periods of data and periods of gaps
 #' @export
 #'
 #' @examples
@@ -32,25 +37,24 @@ gr_get_gaps <- function(hdata) {
     dplyr::rename(Date = 1) %>% 
     dplyr::filter(!is.na(Date)) %>% 
     tidyr::complete(Date = seq(min(Date, na.rm = T), max(Date, na.rm = T), by = 'day')) %>% 
-    mutate(type = if_else(complete.cases(.[-1]), 'data', 'gap'),
-           num = with(rle(type), rep(seq_along(lengths), lengths))) %>% 
-    group_by(num) %>% 
-    summarise(start_date = min(Date),
-              end_date = max(Date),
-              duration = end_date - start_date + 1,
-              type = first(type))
+    dplyr::mutate(type = if_else(complete.cases(.[-1]), 'data', 'gap'),
+                  num = with(rle(type), rep(seq_along(lengths), lengths))) %>% 
+    dplyr::group_by(num) %>% 
+    dplyr::summarise(start_date = min(Date),
+                     end_date = max(Date),
+                     duration = end_date - start_date + 1,
+                     type = dplyr::first(type))
 }
   
-#' Fill missing discharge data using linear interpolation
+#' Fill missing runoff data
+#' 
+#' Use the function to fill the missing runoff data by linear interpolation. These can be both missing dates and missing runoff values. A preliminary summary of missing data can be viewed by [grwat::gr_get_gaps()]
 #'
-#' @param hdata Water discharge series. Data frame containing 4 columns: year (YYYY), montth (MM), day (DD) and level (water level)
-#' @param autocorr Autocorrelation value that defines possible length of the period. Defaults to 0.7.  If `nobserv` parameter is set, then this parameter is ignored. If both parameters are `NULL`, then all gaps are filled disregard of their lengths (not recommended).
-#' @param nobserv Maximum number of contiguous observations that can be filled. Defaults to `NULL`. If this parameter is set by the user, then `autocorr` parameter is ignored. If both parameters are `NULL`, then all gaps are filled disregard of their lengths (not recommended).
-#' @param expand Should the algorithm insert missing dates?
-#' @param dates 
-#' @param order 
+#' @param hdata `data.frame` with two columns, where the first column is `Date`, and the second column is numeric runoff value.
+#' @param autocorr Autocorrelation value that defines possible length of the period that can be filled. Defaults to 0.7.  If `nobserv` parameter is set, then this parameter is ignored. If both parameters are `NULL`, then all gaps are filled disregard of their lengths (not recommended).
+#' @param nobserv Maximum number of contiguous observations that can be interpolated. Defaults to `NULL`. If this parameter is set, then `autocorr` parameter is ignored. If both parameters are `NULL`, then all gaps are filled disregard of their lengths (not recommended).
 #'
-#' @return filled discharge values
+#' @return `data.frame` which is a filled version of `hdata`
 #' @export
 #'
 #' @examples
@@ -62,46 +66,48 @@ gr_fill_gaps <- function(hdata, autocorr = 0.7, nobserv = NULL) {
   nms = colnames(hdata)[-1]
   
   tab = hdata %>% 
-    rename(Date = 1) %>% 
+    dplyr::rename(Date = 1) %>% 
     dplyr::filter(!is.na(Date)) %>% 
     tidyr::complete(Date = seq(min(Date, na.rm = T), max(Date, na.rm = T), by = 'day'))
   
   # Calculate via autocorrelation
   if (is.null(nobserv)) {
     timerep = tab %>% 
-      mutate(type = if_else(complete.cases(tab[-1]), 'data', 'gap'),
+      dplyr::mutate(type = dplyr::if_else(complete.cases(tab[-1]), 'data', 'gap'),
              num = with(rle(type), rep(seq_along(lengths), lengths))) %>% 
-      group_by(num) %>% 
-      summarise(start_date = min(Date),
+      dplyr::group_by(num) %>% 
+      dplyr::summarise(start_date = min(Date),
                 end_date = max(Date),
                 duration = end_date - start_date + 1,
-                type = first(type))
+                type = dplyr::first(type))
     
     max_period = dplyr::filter(timerep, type == 'data', duration == max(duration))
     
-    tab_afun = dplyr::filter(tab, between(Date, max_period$start_date, max_period$end_date))
+    tab_afun = dplyr::filter(tab, dplyr::between(Date, max_period$start_date, max_period$end_date))
     
     nobserv = sapply(2:ncol(tab_afun), function(i) {
       afun = tab_afun %>% 
-        pull(i) %>% 
+        dplyr::pull(i) %>% 
         acf(plot = FALSE)
       
       purrr::detect_index(afun$acf, ~ .x < autocorr) - 1
-    }) |> setNames(nms)
+    }) %>% setNames(nms)
   } else {
     nvars = ncol(hdata)-1
     if (length(nobserv) == 1) {
-      nobserv = as.list(rep(nobserv, nvars)) |> setNames(nms)
+      nobserv = as.list(rep(nobserv, nvars)) %>% setNames(nms)
     } else if (length(nobserv) == nvars) {
-      nobserv = as.list(nobserv) |> setNames(nms)
+      nobserv = as.list(nobserv)%>% setNames(nms)
     } else {
       stop(crayon::white$bgRed$bold('grwat:'), ' nobserv parameter should have length 1 or ', 
            length(df)-1, ' but ', length(nobserv), ' elements are given.')
     }
   }
   
-  tabres = tab |> 
-    mutate(across(2:ncol(tab), ~zoo::na.approx(tab[[cur_column()]], maxgap = nobserv[cur_column()], na.rm = FALSE))) |> 
+  tabres = tab %>%
+    dplyr::mutate(across(2:ncol(tab), 
+                         ~zoo::na.approx(tab[[dplyr::cur_column()]], 
+                                         maxgap = nobserv[dplyr::cur_column()], na.rm = FALSE))) %>% 
     setNames(colnames(hdata))
   
   message(crayon::white$bold('grwat:'), ' filled ', 
@@ -112,20 +118,20 @@ gr_fill_gaps <- function(hdata, autocorr = 0.7, nobserv = NULL) {
   
 }
 
-#' Join grwat reanalysis to hydrological time series
+#' Join reanalysis data
+#' 
+#' The function performs spatial join of meteorological variables (temperature and precipitation) from [grwat reanalysis](http://carto.geogr.msu.ru/grwat/) to the daily runoff time series. Reanalysis covers the East European Plain with 0.75 degrees spatial resolution and is obtained based on CIRES-DOE (1880-1949) and ERA5 (1950-2021) data. This function is useful when the data from meteorological stations are missing inside the basin.
+#' 
+#' Download the reanalysis archive from http://carto.geogr.msu.ru/grwat/.
 #'
-#' @param hdata data frame containing 2 columns: date and discharge level
-#' @param rean List. Contains three components: days series, values and location points
-#' @param buffer Region to select reanalysis data
+#' @param hdata `data.frame` containing 2 columns: `Date` and runoff
+#' @param rean `list` as returned by [grwat::gr_read_rean()]
+#' @param buffer `sf` object containing the region to select reanalysis data. Usually a river basin is used to select the meteorological data. Use [grwat::gr_buffer_geo()] to buffer the basin by specified distance and get more data, if needed.
 #'
-#' @return List of two components: water level table enriched with reanalysis data
-#'   and selected points
+#' @return `data.frame` with four columns: date, runoff, temperature, precipitation.
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' grwat::process(hdata, rean, buffer)
-#' }
 gr_join_rean <- function(hdata, rean, buffer){
   
   # determine the first and last date
@@ -135,10 +141,6 @@ gr_join_rean <- function(hdata, rean, buffer){
   # generate sequence of dates
   hdates = seq(date_first, date_last, "days")
   ndates = length(hdates)
-  
-  # frame for visualizing points
-  # frame = sf::st_bbox(buffer) %>% as.numeric()
-  # frame_sf = sf::st_bbox(buffer) %>% st_as_sfc()
    
   # Select points
   pts_selected = rean$pts[buffer, ]
