01984f23da6bcfc9a122b255f513ae44c447f83e
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2022-04-13 14:39:38 +0300

    add example to the gr_plot_tests() function

---
+++ inst/examples/gr_plot_tests.R
@@ -0,0 +1,15 @@
+library(grwat)
+
+data(spas) # example Spas-Zagorye data is included with grwat package
+
+# separate
+sep = gr_separate(spas, params = gr_get_params(reg = 'Midplain'))
+
+# summarize
+vars = gr_summarize(sep)
+
+# test all variables
+tests = gr_test_vars(vars)
+
+# plot change year from Pettitt test
+gr_plot_tests(tests, type = 'year')
