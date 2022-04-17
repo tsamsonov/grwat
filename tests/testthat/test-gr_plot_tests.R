acbc328e21162ed98bde2196d3899f37e0ec0e60
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2022-04-17 15:16:22 +0300

    add tests for gr_plot_tests()

---
+++ tests/testthat/test-gr_plot_tests.R
@@ -0,0 +1,16 @@
+data(spas) 
+
+sep = gr_separate(spas, params = gr_get_params(reg = 'Midplain'))
+vars = suppressWarnings(gr_summarize(sep))
+tests = suppressWarnings(gr_test_vars(vars))
+
+test_that("Tests plot has the correct content", {
+  
+  plt = gr_plot_tests(tests, type = 'year')
+  expect_type(plt, 'list')
+  expect_s3_class(plt, 'ggplot')
+  expect_gte(plt[["layers"]][[3]][["data"]][["xintercept"]], 1978)
+  expect_lte(plt[["layers"]][[3]][["data"]][["xintercept"]], 1979)
+  
+})
+
