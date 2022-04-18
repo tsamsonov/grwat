17021ce00078f7a2193a22d2fa4e1a969a2ce451
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2022-04-19 00:43:42 +0300

    update help to gr_set_locale()

--- R/locale.R
+++ R/locale.R
@@ -2,20 +2,36 @@ grenv = new.env(parent = emptyenv())
 grenv$loc = 'EN'
 
 #' Set the language that is used for plotting
+#' 
+#' Run this function once at the beginning of the session. All plots will be labeled using the selected language.
+#' 
+#' Note to Linux users: the desired locale may not be installed on the system. A list of available locales can be obtained in bash terminal:
+#' 
+#' `locale -a`
+#' 
+#' Russian locale is `ru_RU.UTF-8`, and Ukrainian locale is `uk_UA.UTF-8`. If absent in the list, then install the desired locales by:
+#' 
+#' `sudo locale-gen ru_RU.UTF-8`
+#' 
+#' `sudo locale-gen uk_UA.UTF-8`
+#' 
+#' `sudo update-locale`
+#' 
+#' Then restart R session, and localization should work as expected.
 #'
-#' @param locale Character string locale. Currently only English (`'EN'`) and Russian (`'RU'`) locales are supported. Defaults to `'EN'`.
+#' @param locale Character string locale. Currently only English (`'EN'`), Russian (`'RU'`) and Ukrainian (`'UA'`) locales are supported. More locales can be requested at issue on GitHub. Defaults to `'EN'`.
 #'
 #' @export
 #'
 #' @example inst/examples/gr_set_locale.R
 #' 
 gr_set_locale <- function(locale = 'EN') {
-  if (locale %in% c('EN', 'RU')) {
+  if (locale %in% c('EN', 'RU', 'UA')) {
     grenv$loc = locale
   } else {
     warning(crayon::white$bgBlue$bold('grwat:'), ' ',
             crayon::white$italic(locale),
-            " locale is not supported yet. Use 'EN' or 'RU' locale.")
+            " locale is not supported yet. Use 'EN', 'RU' or 'UA' locale.")
   }
 }
 
